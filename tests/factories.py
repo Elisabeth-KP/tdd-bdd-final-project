29cf80401ea8c413648da0037f9c1782d6095462
Elisabeth-KP <elisabeth.pedro@ideiasdinamicas.com>
2026-04-27 12:19:27 +0100

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -22,13 +22,40 @@ from factory.fuzzy import FuzzyChoice, FuzzyDecimal
 from service.models import Product, Category
 
 
- class AccountFactory(factory.Factory):
-        """ Creates fake Accounts """
-        class Meta:
-            model = Account
-        id = factory.Sequence(lambda n: n)
-        name = factory.Faker("name")
-        email = factory.Faker("email")
-        phone_number = factory.Faker("phone_number")
-        disabled = FuzzyChoice(choices=[True, False])
-        date_joined = FuzzyDate(date(2008, 1, 1))
+class ProductFactory(factory.Factory):
+    """Creates fake products for testing"""
+
+    class Meta:
+        """Maps factory to data model"""
+
+        model = Product
+
+    id = factory.Sequence(lambda n: n)
+    name = FuzzyChoice(
+        choices=[
+	        "Hat",
+	        "Pants",
+	        "Shirt",
+	        "Apple",
+	        "Banana",
+	        "Pots",
+	        "Towels",
+	        "Ford",
+	        "Chevy",
+	        "Hammer",
+	        "Wrench"
+	    ]
+    )
+    description = factory.Faker("text")
+    price = FuzzyDecimal(0.5, 2000.0, 2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(
+        choices=[
+            Category.UNKNOWN,
+            Category.CLOTHS,
+            Category.FOOD,
+            Category.HOUSEWARES,
+            Category.AUTOMOTIVE,
+            Category.TOOLS,
+        ]
+    )
